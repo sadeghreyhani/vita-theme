778d5575bd0293d00eae325465738ac7f90e7f9b
sadeghreyhani <115707732+sadeghreyhani@users.noreply.github.com>
2022-10-25 13:56:28 +0330

    fix hamburger menu

--- assets/scss/main.js
+++ assets/scss/main.js
@@ -1,6 +1,6 @@
-var x = document.getElementsByClassName("c-hamburger__icon--open");
-var y = document.getElementsByClassName("c-hamburger__content");
-var z = document.getElementsByClassName("c-hamburger__icon--close");
+var x = document.getElementsByClassName("c-nav__mobile--open");
+var y = document.getElementsByClassName("c-nav__item");
+var z = document.getElementsByClassName("c-nav__mobile--close");
 
 x[0].addEventListener("click", function () {
   y[0].style.transform = "translateY(0)";
